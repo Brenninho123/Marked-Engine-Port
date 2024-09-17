d695050e512a5fc01c5b8eac98f6722b2ca15bfd
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2024-09-17 19:16:28 -0300

    Update SUtil.hx

--- source/mobile/SUtil.hx
+++ source/mobile/SUtil.hx
@@ -153,8 +153,8 @@ class SUtil
 enum abstract StorageType(String) from String to String
 {
 	final forcedPath = '/storage/emulated/0/';
-	final packageNameLocal = 'com.shadowmario.psychengine';
-	final fileLocal = 'PsychEngine';
+	final packageNameLocal = 'com.markedaman.markedengine';
+	final fileLocal = 'MarkedEngine';
 
 	public static function fromStr(str:String):StorageType
 	{
